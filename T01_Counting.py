a626aeecbdb2b8acc2c47d26e745a1dd34906c3a
Rajat Dhane <R.R.Dhane@sms.ed.ac.uk>
2023-01-19 10:57:48 +0000

    Code tested and output is good

---
+++ T01_Counting.py
@@ -0,0 +1,8 @@
+# Loop across every week in this year and every day in the week, we could use a nested for loop
+for week in range(1,53):
+    print("No of week", week)
+
+    for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]:
+        print(day)
+
+
